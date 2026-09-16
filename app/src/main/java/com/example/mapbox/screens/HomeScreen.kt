62afec3a0d5dcff75e8eaa108adecc887bed32bd
hamzamallick <mohdhamza1928@gmail.com>
2026-09-16 23:33:13 +0530

    checking

--- app/src/main/java/com/example/mapbox/screens/HomeScreen.kt
+++ app/src/main/java/com/example/mapbox/screens/HomeScreen.kt
@@ -2,7 +2,9 @@ package com.example.mapbox.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -11,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -21,6 +24,11 @@ fun HomeScreen(onOpenMap: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
+
+        Text("Click button to explore the world..!")
+
+        Spacer(Modifier.height(30.dp))
 
         Button(
             onClick = {
